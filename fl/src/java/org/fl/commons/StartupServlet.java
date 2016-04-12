9424d74eb644d1f090ea4e99b2ffc3c751af85bc
Saul Correas Subias <saul.correas@gmail.com>
2016-04-12 23:32:29 +0200

    Salvaguarda

--- fl/src/java/org/fl/commons/StartupServlet.java
+++ fl/src/java/org/fl/commons/StartupServlet.java
@@ -1,7 +1,12 @@
 package org.fl.commons;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.sql.DataSource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -21,6 +26,15 @@ public class StartupServlet extends HttpServlet {
     public void init() throws ServletException {
         log.info("-----> Inicio");
         
+        Connection c = null;
+        try {
+            System.err.println("HOLA TONTO");
+            InitialContext ic = new InitialContext();
+            DataSource ds = (DataSource) ic.lookup("java:comp/env/jdbc/flDS");
+            c = ds.getConnection();
+        } catch(NamingException | SQLException e) {
+            log.error(e.getStackTrace());
+        }
                 
         log.info("<----- Fin");
     }
