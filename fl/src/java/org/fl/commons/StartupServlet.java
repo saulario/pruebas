c1478845e86959322298a495943930a3bdd93cec
Saul Correas Subias <saul.correas@gmail.com>
2016-04-14 00:09:02 +0200

    Savepoint

--- fl/src/java/org/fl/commons/StartupServlet.java
+++ fl/src/java/org/fl/commons/StartupServlet.java
@@ -1,7 +1,9 @@
 package org.fl.commons;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.servlet.ServletException;
@@ -15,27 +17,32 @@ import org.apache.logging.log4j.Logger;
  * @author saulario
  */
 public class StartupServlet extends HttpServlet {
-    
+
     public static final Logger log = LogManager.getLogger();
 
     /**
-     * 
-     * @throws ServletException 
+     *
+     * @throws ServletException
      */
     @Override
     public void init() throws ServletException {
         log.info("-----> Inicio");
-        
+
         Connection c = null;
         try {
-            System.err.println("HOLA TONTO");
-            InitialContext ic = new InitialContext();
-            DataSource ds = (DataSource) ic.lookup("java:comp/env/jdbc/flDS");
+            Context ic = new InitialContext();
+            DataSource ds = (DataSource) ic.lookup("java:/comp/env/jdbc/flDS");
             c = ds.getConnection();
-        } catch(NamingException | SQLException e) {
-            log.error(e.getStackTrace());
+
+            PreparedStatement ps = c.prepareStatement("select * from pepito");
+            ps.executeQuery();
+            ps.close();
+
+            c.close();
+        } catch (NamingException | SQLException e) {
+            log.error(this, e);
         }
-                
+
         log.info("<----- Fin");
     }
 
