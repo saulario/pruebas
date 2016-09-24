facdba018f1f1f6e8b7a85367147b25ca897b754
saulario <saul.correas@gmail.com>
2016-09-24 13:37:35 +0200

    Salvaguarda

--- csoft/csoft/src/main.cpp
+++ csoft/csoft/src/main.cpp
@@ -16,33 +16,36 @@
 #include "Csoft.h"
 
 #ifndef DBDIR
-#define DBDIR "."
+#define DBDIR "./"
 #endif
 #ifndef SYSCONFDIR
-#define SYSCONFDIR "."
+#define SYSCONFDIR "./"
 #endif
 
 
 void init_log(void) {
-    
     std::string logger_ini(SYSCONFDIR);
     logger_ini += "/logger.ini";
     
     std::ifstream file(logger_ini, std::ifstream::in);
     boost::log::init_from_stream(file);    
+    boost::log::add_common_attributes();
+
+}
+
+bool environment_ok() {
+    std::cerr << "csoft:\tchecking environment" << std::endl;
     
-//    boost::log::add_file_log(
-//            boost::log::keywords::file_name = "/usr/local/log/csoft_%N.log",
-//            boost::log::keywords::rotation_size = 1024 * 1024 * 10,
-//            boost::log::keywords::time_based_rotation = boost::log::sinks::file::rotation_at_time_point(0, 0, 0),
-//            boost::log::keywords::format = "[%TimeStamp%][%ThreadID%]: %Message%"
-//            );
-//    
-//    boost::log::core::get()->set_filter(
-//            boost::log::trivial::severity >= boost::log::trivial::info
-//            );
-//    
-//    boost::shared_ptr<boost::log::core> core = boost::log::core::get();
+    std::cerr << "\tLogger configuration...";
+    
+    init_log();    
+    std::cerr << "OK" << std::endl;
+    
+    std::cerr << "\tDatabase storage...";
+    
+    std::cerr << "OK" << std::endl;
+    
+    return true;
 }
 
 boost::log::sources::severity_logger<boost::log::trivial::severity_level> lg;
@@ -56,20 +59,12 @@ void signal_handler(int signo) {
 }
 
 int main(int argc, char **argv) {
-   
-    init_log();
-    boost::log::add_common_attributes();
     
-
+    if (!environment_ok()) {
+        return EXIT_FAILURE;
+    }
     
     BOOST_LOG_SEV(lg, boost::log::trivial::info) << __PRETTY_FUNCTION__ << "---> Begin";
-
-    BOOST_LOG_SEV(lg, boost::log::trivial::trace) << "\ttrace";
-    BOOST_LOG_SEV(lg, boost::log::trivial::debug) << "\tdebug";
-    BOOST_LOG_SEV(lg, boost::log::trivial::info) << "\tinfo";
-    BOOST_LOG_SEV(lg, boost::log::trivial::error) << "\terror";
-    BOOST_LOG_SEV(lg, boost::log::trivial::fatal) << "\tfatal";
-
 
     if (signal(SIGHUP, signal_handler) == SIG_ERR) {
         printf("Registrando SIGHUP\n");
@@ -79,29 +74,13 @@ int main(int argc, char **argv) {
         printf("Registrando SIGINT\n");
         return EXIT_FAILURE;
     }
-    /*
-        if (signal(SIGKILL, signal_handler) == SIG_ERR) {
-            printf("Registrando SIGKILL\n");
-            return EXIT_FAILURE;
-        }
-     */
     if (signal(SIGTERM, signal_handler) == SIG_ERR) {
         printf("Registrando SIGTERM\n");
         return EXIT_FAILURE;
     }
 
-    //    while(1) {
-    //        sleep(1000);
-    //        break;
-    //    }
-
-
     csoft::Csoft csoft;
     int retval = csoft.doIt(argc, argv);
-
-    BOOST_LOG_SEV(lg, boost::log::trivial::info) << "\t(HOME): " << getenv("HOME");
-
-
 
     BOOST_LOG_SEV(lg, boost::log::trivial::info) << __PRETTY_FUNCTION__ << "<--- End (retval): " << retval;
     boost::log::core::get()->remove_all_sinks();
